3cca01880f0e57c20d56643b168539d65a60143b
CaweAlves <contato@cawe.dev>
2025-03-03 09:33:27 -0300

    add a simple configmap example

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from Backend!'
+    greeting_message = os.getenv('GREETING_MESSAGE', 'Hello, World from Backend!')
+    return greeting_message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
